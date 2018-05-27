ad340138d557f4860c78bc4079700be2a2210c94
liuyumeng <308591952@qq.com>
2018-05-27 21:55:24 +0800

    JDK-动态代理实例

--- src/com/base/proxy/jdkproxy/ProxyTest.java
+++ src/com/base/proxy/jdkproxy/ProxyTest.java
@@ -8,7 +8,9 @@ package com.base.proxy.jdkproxy;
  * ---------after-----------
  *
  * 原理分析：
+ * http://rejoy.iteye.com/blog/1627405(写的非常详尽了)
  *
+ * “jdk动态代理之所以只能代理接口是因为代理类本身已经extends了Proxy，而java是不允许多重继承的，但是允许实现多个接口，因此才有cglib的需要吧”
  */
 public class ProxyTest {
     public static void main(String[] args) {
